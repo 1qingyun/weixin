7650527e3759194f4375a4eb682911b479502e77
1qingyun <1871151141@qq.com>
2017-05-15 13:29:00 +0800

    Update weixin.js

--- js/weixin.js
+++ js/weixin.js
@@ -3,4 +3,7 @@
  */
 $(function(){
   $('p.title').addClass('animated bounceInDown')
+  $('p.first').addClass('animated slideInUp')
+  $('p.second').addClass('animated fadeInLeft')
+  $('p.third').addClass('animated flipInY')
 });
